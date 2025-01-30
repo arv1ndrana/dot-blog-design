6b1e50f742d554f264977a4d35572c9148dbf24d
Arvind Rana <arvindrana6969@gmail.com>
2025-01-30 16:16:51 +0545

    added varied length dots in blogs

--- main.js
+++ main.js
@@ -4,15 +4,25 @@ const blogsNumber = document.getElementById("blog-list").childElementCount;
 for (let i = 0; i < blogsNumber; i++)
 {
 
-    //To select a blog
+    // To select a blog
     const selectedBlog = 'div.blog' + (i + 1).toString();
+    console.log("BLOG: " + selectedBlog);
 
+    // TODO: To count the inner content of name and date id
+
+
+    const nameLength = document.querySelectorAll(selectedBlog + " > span")[0].textContent.length;
+    const dateLength = document.querySelectorAll(selectedBlog + " > span")[2].textContent.length;
+    console.log("TITLE LENGTH: " + nameLength)
+    console.log("DATE LENGTH: " + dateLength)
     // const nameLength = document.querySelectorAll('#name')[0].textContent.length;
     // const dateLength = document.querySelectorAll('#date')[0].textContent.length;
     
-    // const totalLength = nameLength + dateLength;
-    // const dotLength = 100 - totalLength;
+    const totalLength = nameLength + dateLength;
+    console.log("TOTAL LENGTH: " + totalLength);
+    const dotLength = 100 - totalLength;
+    console.log("DOT LENGTH: " + dotLength);
     
     
-    // document.getElementById("dots").textContent = " . ".repeat(dotLength);
+    document.querySelectorAll(selectedBlog + " > span")[1].textContent = " . ".repeat(dotLength);
 }
